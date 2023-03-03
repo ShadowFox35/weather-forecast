f601921f3a1286dee29a03f7b125286c6df80e5c
ShadowFox35 <anastasia97chernova@gmail.com>
2023-03-03 16:12:20 +0100

    feat: getData function for added city

--- weather-forecast/src/components/Header/Search/Search.tsx
+++ weather-forecast/src/components/Header/Search/Search.tsx
@@ -11,17 +11,30 @@ const Search: React.FC = () => {
   const citiesArray = useSelector((state: any) => state.citiesArrayRedicer.citiesArray);
   const [inputCity, setInputCity] = useState<string>('');
 
-  const enterCity = (event: React.KeyboardEvent<HTMLElement>) => {
-    if (event.key === 'Enter') {
-      let list = [...citiesArray];
-      list.length === 5 && list.pop();
-      !citiesArray.includes(inputCity) && list.unshift(inputCity);
+  const API_KEY = 'b69290eb7d314300a97120031232802';
 
+  const getData = async (request: string) => {
+    let list = [...citiesArray];
+    const result = await fetch(
+      `http://api.worldweatheronline.com/premium/v1/weather.ashx?q=${request}&num_of_days=1&key=${API_KEY}&format=json`
+    ).then((response) => {
+      return response.json();
+    });
+
+    if (result.data.request[0].query) {
+      console.log(result.data);
+      console.log(result.data.request[0].query);
+      list.length === 5 && list.pop();
+      !citiesArray.includes(result.data.request[0].query) && list.unshift(result.data.request[0].query);
       dispatch(addCity(list));
     }
   };
 
-  console.log('citiesArray', citiesArray);
+  const handleEnterCity = (event: React.KeyboardEvent<HTMLElement>) => {
+    if (event.key === 'Enter') {
+      getData(inputCity);
+    }
+  };
 
   const readInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputCity(event.target.value);
@@ -38,11 +51,18 @@ const Search: React.FC = () => {
           name="search"
           value={inputCity}
           tabIndex={0}
-          onKeyDown={enterCity}
+          onKeyDown={handleEnterCity}
           onChange={readInput}
         />
         <div className="search_icon">
-          <img src={search} alt="search icon" className="icon" />
+          <img
+            src={search}
+            alt="search icon"
+            className="icon"
+            onClick={() => {
+              getData(inputCity);
+            }}
+          />
         </div>
       </div>
     </section>
