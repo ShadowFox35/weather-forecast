62243e33c99385ef37c4d2b9d1a6737e4e8933aa
ShadowFox35 <anastasia97chernova@gmail.com>
2023-03-08 21:36:29 +0100

    fix: remove icon onClick

--- weather-forecast/src/components/Header/Cities/Cities.tsx
+++ weather-forecast/src/components/Header/Cities/Cities.tsx
@@ -17,37 +17,46 @@ const Cities: React.FC = () => {
   const normalizeCity = (data: forecastElemType) => data.request[0].query.split(' ')[0].replace(',', '');
 
   const deleteCity = (city: string, index: number) => {
+    console.log(111);
+
     if (forecastArray.length > 1) {
       let list = forecastArray.filter((data: forecastElemType) => data.request[0].query !== city);
       dispatch(editForecastArray(list));
       localStorage.setItem('citiesForecast', JSON.stringify(list));
+      console.log(activeForecast !== 0 && index <= activeForecast);
 
       if (activeForecast !== 0 && index <= activeForecast) {
-        dispatch(editActiveForecast(activeForecast - 1));
+        console.log('result ', activeForecast - 1);
+        const newIndex = activeForecast - 1;
+        dispatch(editActiveForecast(newIndex));
       }
     }
   };
+  console.log('asd', activeForecast);
 
   const chooseCity = (index: number) => {
+    console.log('pidor');
+
     dispatch(editActiveForecast(index));
   };
 
   return (
     <section className="cities">
       {forecastArray.map((data: forecastElemType, index: number) => (
-        <div className={clsx('city', { active: index === activeForecast })} key={index}>
-          <h2
-            className="city_name"
-            onClick={() => {
-              chooseCity(index);
-            }}>
-            {normalizeCity(data) || ''}{' '}
-          </h2>
+        <div
+          className={clsx('city', { active: index === activeForecast })}
+          key={index}
+          onClick={() => {
+            chooseCity(index);
+          }}>
+          <h2 className="city_name">{normalizeCity(data) || ''} </h2>
           <img
             className={clsx('city_remove-icon', { disable: forecastArray.length === 1 })}
             src={removeIcon}
             alt="remove icon"
-            onClick={() => {
+            onClick={(event: any) => {
+              event.preventDefault();
+              event.stopPropagation();
               deleteCity(data.request[0].query, index);
             }}></img>
         </div>
